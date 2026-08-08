65d28c121ac5b5b1946a61f364f4ba9c15e60019
Trung <vmt162005@gmail.com>
2026-08-08 15:19:40 +0700

    update: config cors api gateway

--- api-gateway/src/main/java/com/trung/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/trung/apigateway/config/CorsConfig.java
@@ -5,12 +5,20 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOriginPattern("*");
+        corsConfig.setAllowedOrigins(List.of(
+                "https://ridehailingsystem.online",
+                "http://localhost",
+                "http://localhost:80",
+                "http://localhost:3000",
+                "http://127.0.0.1:8080"
+        ));
         corsConfig.addAllowedMethod("*");
         corsConfig.addAllowedHeader("*");
         corsConfig.setAllowCredentials(true);
